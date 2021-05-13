75695fda1816af3221166069ba3b196db1fcee72
Alex Hladun <alexhladun@gmail.com>
2021-05-13 00:22:01 -0600

    Working, but with offset

--- client/src/components/SketchPad/SketchPad.tsx
+++ client/src/components/SketchPad/SketchPad.tsx
@@ -8,7 +8,7 @@ interface Props {}
 const SketchPad: React.FC<Props> = ({}) => {
   const socket = useContext(SocketContext);
   const [color, setColor] = useState("#1362b0");
-  const [strokeWidth, setStrokeWidth] = useState(10);
+  const [strokeWidth, setStrokeWidth] = useState(1);
 
   const sendMouse = (x, y, pX, pY) => {
     const data = {
@@ -26,11 +26,22 @@ const SketchPad: React.FC<Props> = ({}) => {
   };
   //See annotations in JS for more information
   const setup = (p5: p5Types, canvasParentRef: Element) => {
-    const cnv = p5.createCanvas(1000, 500).parent(canvasParentRef);
-    cnv.mousePressed((event) => {});
+    p5.createCanvas(1000, 500).parent(canvasParentRef);
     p5.mouseDragged = (event: any) => {
-      const { pageX, pageY, x, y } = event;
-      sendMouse(x, y, pageX, pageY);
+      console.log("🚀 ~ file: sketchPad.tsx ~ line 32 ~ setup ~ event", event);
+      const {
+        offsetX,
+        offsetY,
+        x,
+        y,
+        px,
+        py,
+        layerX,
+        layerY,
+        movementX,
+        movementY
+      } = event;
+      sendMouse(x, y, x - movementX, y + movementY);
     };
   };
 
@@ -43,16 +54,15 @@ const SketchPad: React.FC<Props> = ({}) => {
   });
 
   const draw = (p5: p5Types) => {
-    // p5.background(1);
+    // p5.background(202, 231, 237);
     // p5.ellipse(x, y, 70, 70);
     // p5.line(40, 60, 80, 200);
     if (socket) {
       socket.on("mouse", (data) => {
-        if (data.color) {
-          p5.stroke("red");
-          p5.strokeWeight(data.strokeWidth);
-          p5.line(data.x, data.y, data.px, data.py);
-        }
+        p5.stroke(data.color);
+        p5.strokeWeight(data.strokeWidth);
+        // p5.point(data.px, data.py);
+        p5.line(data.px, data.py, data.x, data.y);
       });
     }
   };
