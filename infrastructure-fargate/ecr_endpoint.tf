646ae1a0894578797d23447b5280354412263f49
Alex Hladun <alexhladun@gmail.com>
2021-05-31 22:36:30 -0600

    move destroy steps

--- infrastructure-fargate/ecr_endpoint.tf
+++ infrastructure-fargate/ecr_endpoint.tf
@@ -1,4 +1,4 @@
-resource "local_file" "instance_ips" {
+resource "local_file" "ecr_endpoint" {
     content     = "${aws_ecr_repository.socket-app-repository.repository_url}"
     filename = "${path.module}/ecr_endpoint.txt"
 }
