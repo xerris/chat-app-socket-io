14d3259c00a6a50b30b5fb0d7f638013bf24e20a
Alex Hladun <alexhladun@gmail.com>
2021-06-06 12:51:41 -0600

    Remove ECR endpoint fromo terraform

--- infrastructure-fargate/ecr.tf
+++ infrastructure-fargate/ecr.tf
@@ -1,30 +1,30 @@
-resource "aws_ecr_repository" "socket-app-repository" {
-  name                 = "xerris-socket-app-repo"
-  image_tag_mutability = "IMMUTABLE"
-}
+# resource "aws_ecr_repository" "socket-app-repository" {
+#   name                 = "xerris-socket-app-repo"
+#   image_tag_mutability = "IMMUTABLE"
+# }
 
-resource "aws_ecr_repository_policy" "socket-app-policy" {
-  repository = aws_ecr_repository.socket-app-repository.name
-  policy     = <<EOF
-  {
-    "Version": "2008-10-17",
-    "Statement": [
-      {
-        "Sid": "Full ECR access to socket-app repository",
-        "Effect": "Allow",
-        "Principal": "*",
-        "Action": [
-          "ecr:BatchCheckLayerAvailability",
-          "ecr:BatchGetImage",
-          "ecr:CompleteLayerUpload",
-          "ecr:GetDownloadUrlForLayer",
-          "ecr:GetLifecyclePolicy",
-          "ecr:InitiateLayerUpload",
-          "ecr:PutImage",
-          "ecr:UploadLayerPart"
-        ]
-      }
-    ]
-  }
-  EOF
-}
+# resource "aws_ecr_repository_policy" "socket-app-policy" {
+#   repository = aws_ecr_repository.socket-app-repository.name
+#   policy     = <<EOF
+#   {
+#     "Version": "2008-10-17",
+#     "Statement": [
+#       {
+#         "Sid": "Full ECR access to socket-app repository",
+#         "Effect": "Allow",
+#         "Principal": "*",
+#         "Action": [
+#           "ecr:BatchCheckLayerAvailability",
+#           "ecr:BatchGetImage",
+#           "ecr:CompleteLayerUpload",
+#           "ecr:GetDownloadUrlForLayer",
+#           "ecr:GetLifecyclePolicy",
+#           "ecr:InitiateLayerUpload",
+#           "ecr:PutImage",
+#           "ecr:UploadLayerPart"
+#         ]
+#       }
+#     ]
+#   }
+#   EOF
+# }
