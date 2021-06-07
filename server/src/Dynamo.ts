aa04559f53b7015bbd12b357e2ddbeae07cbfec2
Alex Hladun <alexhladun@gmail.com>
2021-06-06 21:43:05 -0600

    UPDATE credentials

--- server/src/Dynamo.ts
+++ server/src/Dynamo.ts
@@ -1,27 +1,16 @@
 var AWS = require("aws-sdk");
 if (process.env.ENV === "dev") {
-  var credentials = new AWS.SharedIniFileCredentials({
-    profile: "dynamo",
-    region: "us-west-2"
-  });
   AWS.config.update({
     region: "us-west-2",
-    endpoint: "https://dynamodb.us-west-2.amazonaws.com",
-    credentials: credentials
+    endpoint: "https://dynamodb.us-west-2.amazonaws.com"
   });
 }
 // May need to put credentials in here once deployed
 if (process.env.ENV === "prod") {
-  var credentials = new AWS.SharedIniFileCredentials({
-    profile: "default",
-    region: "us-east-2"
-  });
-
   try {
     AWS.config.update({
       region: "us-east-2",
-      endpoint: "https://dynamodb.us-east-2.amazonaws.com",
-      credentials: credentials
+      endpoint: "https://dynamodb.us-east-2.amazonaws.com"
     });
   } catch (err) {
     console.log("DYNAMO err", err);
