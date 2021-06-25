c3f6238fb05a6f3b027acbb53eb491dd4b3ca47f
Alex Hladun <alexhladun@gmail.com>
2021-06-24 22:19:21 -0600

    try without key_name

--- infrastructure-fargate/nat-instance.tf
+++ infrastructure-fargate/nat-instance.tf
@@ -59,14 +59,12 @@ resource "aws_security_group" "nat" {
 
 }
 
-
-
 resource "aws_instance" "nat" {
-  ami                    = "ami-001e4628006fd3582" # this is a special ami preconfigured to do NAT
-  availability_zone      = data.aws_availability_zones.available.names[count.index]
-  count                  = var.az_count
-  instance_type          = "t2.micro"
-  key_name               = "[default]"
+  ami               = "ami-001e4628006fd3582" # this is a special ami preconfigured to do NAT
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+  count             = var.az_count
+  instance_type     = "t2.micro"
+  # key_name               = "[default]"
   vpc_security_group_ids = ["${aws_security_group.nat.id}"]
   subnet_id              = element(aws_subnet.public.*.id, count.index)
   # subnet_id                   = element(aws_subnet.public.*.id, 0)
